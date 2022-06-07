2e9501c43989957d994bdcf7b1a9fe99e15a0bab
mohab58977 <105084710+mohab58977@users.noreply.github.com>
2022-06-07 06:30:09 +0200

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-2.c4bfhoqxvgqd.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagrambucket5897
+export AWS_REGION=US East (N. Virginia) us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
