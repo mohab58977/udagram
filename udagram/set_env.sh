654135d404738aaac753b75527ca78ed5a484f07
mohab58977 <105084710+mohab58977@users.noreply.github.com>
2022-06-12 04:43:00 +0200

    2nd commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,12 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-2.c4bfhoqxvgqd.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-1.c4bfhoqxvgqd.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::udagrambucket5897
-export AWS_REGION=US East (N. Virginia) us-east-1
+export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
+
+eb setenv POSTGRES_USERNAME=postgres  POSTGRES_PASSWORD=postgres POSTGRES_HOST=database-1.c4bfhoqxvgqd.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres AWS_BUCKET=arn:aws:s3:::udagrambucket5897 AWS_REGION=us-east-1 AWS_PROFILE=default JWT_SECRET=mysecretstring URL=http://localhost:8100
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: 'udagram',
+  apiHost:
+    'http://udagram-env.eba-8qp5vn37.us-east-1.elasticbeanstalk.com/api/v0/',
 };
 
 /*
