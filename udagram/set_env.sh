b5191cf278e26199d8b953389262cc2bd0d55807
mohab58977 <105084710+mohab58977@users.noreply.github.com>
2022-06-13 03:09:50 +0200

    2nd commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -4,10 +4,10 @@ export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
 export POSTGRES_HOST=database-1.c4bfhoqxvgqd.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::udagrambucket5897
+export AWS_BUCKET=udagrambucket5897
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://udagrambucket5897.s3-website-us-east-1.amazonaws.com/
 
-eb setenv POSTGRES_USERNAME=postgres  POSTGRES_PASSWORD=postgres POSTGRES_HOST=database-1.c4bfhoqxvgqd.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres AWS_BUCKET=arn:aws:s3:::udagrambucket5897 AWS_REGION=us-east-1 AWS_PROFILE=default JWT_SECRET=mysecretstring URL=http://localhost:8100
+eb setenv POSTGRES_USERNAME=postgres  POSTGRES_PASSWORD=postgres POSTGRES_HOST=database-1.c4bfhoqxvgqd.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres AWS_BUCKET=udagrambucket5897 AWS_REGION=us-east-1 AWS_PROFILE=default JWT_SECRET=mysecretstring URL=http://localhost:8100
